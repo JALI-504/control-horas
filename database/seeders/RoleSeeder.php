b5eeea1c499ee497cb28fddd8b7c52a3c3243607
unknown <furio504@hotmail.com>
2023-05-10 08:32:38 -0600

    antes de implenentar roles y permisos

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -19,18 +19,20 @@ class RoleSeeder extends Seeder
         $role1 = Role::create(['name' => 'Admin']);
         $role2 = Role::create(['name' => 'Practicante']);
 
-        Permission::create(['name' => 'admin.hp.index']);
+        Permission::create(['name' => 'admin.hp.index'])->syncRoles([$role1, $role2]);
 
-        Permission::create(['name' => 'admin.practicantes.hp.practicante']);           
-        Permission::create(['name' => 'admin.practicantes.hp.create']);           
-        Permission::create(['name' => 'admin.practicantes.hp.update']);                    
-        Permission::create(['name' => 'admin.practicantes.destroy']);  
+        Permission::create(['name' => 'admin.practicantes.hp.practicante'])->syncRoles([$role1, $role2]);           
+        Permission::create(['name' => 'admin.practicantes.hp.create'])->syncRoles([$role1, $role2]);           
+        Permission::create(['name' => 'admin.practicantes.hp.update'])->syncRoles([$role1, $role2]);                    
+        Permission::create(['name' => 'admin.practicantes.destroy'])->syncRoles([$role1, $role2]);  
         
         
-        Permission::create(['name' => 'admin.horas.hp.hora']);           
-        Permission::create(['name' => 'admin.horas.hp.hora_create']);           
-        Permission::create(['name' => 'admin.horas.hp.hora_update']);                    
-        Permission::create(['name' => 'admin.horas.destroy']);                    
+        Permission::create(['name' => 'admin.horas.hp.hora'])->syncRoles([$role1, $role2]);           
+        Permission::create(['name' => 'admin.horas.hp.hora_create'])->syncRoles([$role1, $role2]);           
+        Permission::create(['name' => 'admin.horas.hp.hora_update'])->syncRoles([$role1, $role2]);                    
+        Permission::create(['name' => 'admin.horas.destroy'])->syncRoles([$role1, $role2]);    
+        
+        
 
     }
 }
